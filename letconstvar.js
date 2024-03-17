e3c173856b5a1917e6376ef46a7d75451989b1b2
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2024-03-18 00:57:38 +0530

    events completed

---
+++ letconstvar.js
@@ -0,0 +1,6 @@
+//let,const are block scope
+//var is functional scope
+//let and const can not be redeclared in the same scope, but
+//var can be redeclared in the same scope
+
+//var variables are hoisted but let and const variables are not hoisted
