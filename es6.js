73557a2ad6a67c045611c370f99730495a1dd8e9
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2024-08-01 01:21:13 +0530

    extra added

--- es6.js
+++ es6.js
@@ -1,10 +1,17 @@
-// let and const Keywords
-// Arrow Functions
-// Multi-line Strings
-// Default Parameters
-// Template Literals
-// Destructuring Assignment
-// Enhanced Object Literals
-// Promises
-// Classes
-// Modules
+//1. let and const Keywords -- let and const variables not hoisted. var variables are hoisted.
+// let const we can not redeclare again in same scope, but var we can.
+//2. Arrow Functions - this implicitly bind with window object. it is not hoisted. this keyword
+//refers to window object.
+//3. Multi-line Strings
+//4. Default Parameters - function parameter we can set default value
+//5. Template Literals - backtick ``
+//6. Destructuring Assignment - rest and spread operator
+//7. Enhanced Object Literals -
+//  var name = "Duke";
+//  var color = "Brown";
+//  var age = 5;
+//  var dog = {name, color, age};
+//  console.log(dog);   { name:"Duke",color:"Brown", age:5}
+//8. Promises
+//9. Classes
+//10. Modules
--- jsWorks.md
+++ jsWorks.md
@@ -1,7 +1,7 @@
 Macro tasks = setTimeOut, setInterval, I/O,
 Micro tasks = promise, callback,async await, process.nextTick()
 
-callback queue - It holds all the functions which are going exeucutes in first in first out order. callback queue consists of microqueue and macro queue
+callback queue - It holds all the functions which are going exeucutes in first in first out order. callback queue consists of micro queue and macro queue
 
 call stack - where Javascript code executes.
 
